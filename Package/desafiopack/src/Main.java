20dadc83da7e631d7d5bc09ae3dd5d5907f960bc
EduuhSouza <146948069+EduuhSouza@users.noreply.github.com>
2025-08-19 14:26:36 -0300

    Add files via upload

---
+++ Package/desafiopack/src/Main.java
@@ -0,0 +1,14 @@
+import desafio.DesafioPack;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hellow World");
+
+        DesafioPack teste = new DesafioPack();
+
+        teste.nome = "Eduarda";
+        teste.idade = 20;
+
+        teste.exibaFicha();
+    }
+}
---
+++ Package/desafiopack/src/desafio/DesafioPack.java
@@ -0,0 +1,12 @@
+package desafio;
+
+public class DesafioPack {
+    public String nome;
+    public int idade;
+
+    public void exibaFicha(){
+        System.out.println("___________________");
+        System.out.printf("\nOlá, meu nome é %s e tenho %s anos", nome, idade);
+        System.out.println("\n___________________");
+    }
+}
